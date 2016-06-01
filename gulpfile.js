7e955653d2250d43124d11d2afe7b2d738ea6931
jslopeza <jasonlopeza95@gmail.com>
2016-05-31 22:23:11 -0400

    Adds browser sync and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var server = require('gulp-server-livereload');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
 	return gulp.src('./app/sass/**/*.scss')
 		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-		.pipe(gulp.dest('./app/css'));
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
+		.pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./app/sass/**/*.scss', ['sass']);
-});
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: {
+      baseDir: './app'
+    }
+  });
 
-gulp.task('webserver', function() {
-	gulp.src('app')
-		.pipe(server({
-			livereload: true,
-			defaultFile: 'index.html',
-			open: true,
-		}))
+  gulp.watch('app/sass/**/*.scss', ['sass']);
+  gulp.watch('app/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('dev', ['sass', 'watch', 'webserver']);
