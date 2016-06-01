ac8f14059bdcf7b6a3549ca1bbfc112cb2cf078c
jslopeza <jasonlopeza95@gmail.com>
2016-06-01 01:54:31 -0400

    Adds navbar

--- app/js/index.js
+++ app/js/index.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
     css3: true,
     easing: 'swing',
+    anchors: ['home', 'about', 'testimonials', 'contact'],
+    menu: '#menu',
     easingcss3: 'ease-out',
     controlArrows: false,
     scrollOverflow: true,
